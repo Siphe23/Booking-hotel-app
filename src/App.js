7eb9a04b0864963a4e547c66c76d24134ea28d10
Siphe23 <siphesihlemzamane@gmai.com>
2024-09-30 21:30:32 +0200

    added file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'; 
 import Home from './pages/Home';
-import Login from './pages/login'; 
-import Register from './pages/register'; 
-import OurHotels from './pages/ourHotels';
-import Booknow from './pages/booknow';
-
+import OurHotels from './pages/ourHotels'; // Ensure the casing matches the file
+import Booknow from './pages/booknow'; // Ensure the casing matches the file
+import Profile from './pages/Profile'; // Import the Profile component
 
 function App() {
   return (
@@ -19,7 +17,6 @@ function App() {
           <Route path="/ourhotels" element={<OurHotels />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/booknow" element={<Booknow />} />
-         
         </Routes>
       </BrowserRouter>
     </Provider>
@@ -27,3 +24,4 @@ function App() {
 }
 
 export default App;
+
